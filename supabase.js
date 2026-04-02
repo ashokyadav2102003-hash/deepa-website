f3b3ad239d7301121434479a22510b319642169a
ashokyadav2102003-hash <ashokyadav2102003@gmail.com>
2026-04-02 14:29:42 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
+// supabase.js — Supabase connection config
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
+const SUPABASE_URL = 'https://htjuyvlaglzchuwpfiyv.supabase.co'   // ← paste your Project URL
+const SUPABASE_KEY = 'sb_publishable_9dwFucxI3EvpoXFbsKTEIA_CaIQCoK7'           // ← paste your Publishable key
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
